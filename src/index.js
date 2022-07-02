1573c5b6cae7507fe99c2df6876326d44c252bb5
Elton Brayner <eltonbrayner1@gmail.com>
2022-07-01 23:39:25 -0300

    Finish tests

--- src/index.js
+++ src/index.js
@@ -48,15 +48,17 @@ app.put('/repositories/:id', (request, response) => {
 app.delete('/repositories/:id', (request, response) => {
   const { id } = request.params;
 
-  const repository = repositories.find((repository) => repository.id === id);
+  const repository = repositories.findIndex(
+    (repository) => repository.id === id,
+  );
 
-  if (!repository) {
+  if (repository < 0) {
     return response.status(404).json({ error: 'Repository not found' });
   }
 
   repositories.splice(repository, 1);
 
-  return response.status(204).json();
+  return response.status(204).send();
 });
 
 app.post('/repositories/:id/like', (request, response) => {
